5a1ec06b70c2f913be3ed63cd9adc0f7679c9fe6
Fawad <aash4037@gmail.com>
2022-03-12 22:42:00 +0000

    testing php component

---
+++ src/test.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php
+include_once "init.php";
+?>
+  <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap 5.0 Css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+    <!-- Google Fonts (Mulish) -->
+    <link href="https://fonts.googleapis.com/css2?family=Mulish&display=swap" rel="stylesheet">
+
+    <!-- Main Css File -->
+    <link rel="stylesheet" href="assets/css/main.css">
+
+</head>
+<body>
+    <?php require '_navvar.php' ?>
+    <div class="container">
+        <h1> Testing nav bar using component php </h1>
+</body>
+</html>
