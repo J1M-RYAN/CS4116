81fde0afe278e57fbd115e199114dc8679e37978
Fawad <aash4037@gmail.com>
2022-03-13 21:34:22 +0000

    footer as component

---
+++ src/footer.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<?php
+include_once "init.php";
+?>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap 5.0 Css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+    <!-- Google Fonts (Mulish) -->
+    <link href="https://fonts.googleapis.com/css2?family=Mulish&display=swap" rel="stylesheet">
+
+    <!-- Main Css File -->
+    <link rel="stylesheet" href="assets/css/main.css">
+
+    <title>Footer</title>
+
+</head>
+
+<body>
+    <!-- / Footer starts here -->
+    <footer class="bg-dark py-5">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-7 mb-3">
+                    <div class="text-center">
+                        <p class="text-muted para-desc mx-auto mt-3">Enjoy your dating on LOVE SPARK and find the best
+                            match for yourself
+                            and make your life memorable.</p>
+
+                        <!--end icon-->
+                    </div>
+                </div>
+                <!--end col-->
+            </div>
+            <!--end row-->
+        </div>
+        <!--end container-->
+    </footer>
+    <footer class="footer-bar bg-dark">
+        <div class="container">
+            <div class="row text-center">
+                <div class="col-sm-12 footer-bar">
+                    <div class="text-sm-left">
+                        <p class="mb-0 footer-text para-desc mx-auto">&copy; 2022 "LOVE SPARK" All rights Reserved.
+                            Design with
+                            <i class="mdi mdi-heart text-danger"></i>.
+                        </p>
+                    </div>
+                </div>
+                <!--end col-->
+            </div>
+            <!--end row-->
+        </div>
+        <!--end container-->
+    </footer>
+    <!-- / Footer ends here -->
+    <!-- Bootstrap 5.0 JS -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
