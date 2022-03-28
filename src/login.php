4492d0880fc398ac63681af137e057f167403ca0
Fawad Shahzad <60967843+faad555@users.noreply.github.com>
2022-03-28 14:18:43 +0100

    header & footer added

--- src/login.php
+++ src/login.php
@@ -15,6 +15,7 @@
 </head>
 
 <body>
+  <?php require './navbar.php'?>
   <div class="login-page">
     <div class="wrapper">
       <div class="title">Login</div>
@@ -51,6 +52,8 @@
         ?>  
     </div>
   </div>
+  <!-- Footer  -->
+  <?php require './footer.php'?>
   <!-- Bootstrap 5.0 JS -->
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
     integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
